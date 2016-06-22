c479993c49599eb423fe743bb9a25634bf6f480b
Zach Beaird <zbbeaird1989@gmail.com>
2016-06-22 16:46:04 -0500

    Find Largest Prime Factor/Started using present tense commit messages

--- problems.js
+++ problems.js
@@ -29,7 +29,7 @@ function findMultiples() {
 
 
 //Even Fibonacci numbers
-function evenFibNum() {
+function evenFib() {
 	var array = [1, 2];
 
 	function fibSeq() {
@@ -52,8 +52,47 @@ function evenFibNum() {
 
 
 
-//Largest Prime Factor
+//Count through every number
+//Target number
+//Multiply numbers less than target
+//If no multiples equal target, store number as prime
 
+
+//Largest Prime Factor
+function largePrimeFactor(product) {
+	function findPrime() {
+		var all        = [],
+		    prime      = [],
+		    counter    = 0;
+
+		//Find all proceding numbers of product divided by 2
+		for (var i = 1; i <= Math.ceil(product / 2); i++) {
+			all.push(i);
+		}
+
+		//Find prime numbers in "all" array
+		for (var j = 0; j < all.length; j++) {
+			var target  = all[j];				
+			for (var k = 0; k < target; k++) {
+				var current = all[k];
+				if (target === 1) {
+					counter += 1;
+				} else if (target % current === 0 && target != current && current != 1) {
+					counter += 1;
+					console.log("Target: " + target + "; Current is " + current +
+						        "; Counter: " + counter);
+				}
+			}
+			if (counter === 0) {
+				prime.push(target);
+			} else {
+				counter = 0;
+			}
+		}
+		console.log(prime);
+	}
+	findPrime();
+}
 
 
 
@@ -63,7 +102,8 @@ DOM
 
 var solutions = {
 	prob1: sum(findMultiples()),
-	prob2: sum(evenFibNum())
+	prob2: sum(evenFib()),
+	prob3: largePrimeFactor(10)
 }
 
 var buttons = document.getElementsByClassName('btn');
