84a5b01c404c7f913096115c9583f0e8c3edefb0
Zach Beaird <zbbeaird1989@gmail.com>
2016-06-22 22:23:01 -0500

    Touching up layout/Optimizing problem 3

--- problems.js
+++ problems.js
@@ -31,7 +31,6 @@ function findMultiples() {
 //Even Fibonacci numbers
 function evenFib() {
 	var array = [1, 2];
-
 	function fibSeq() {
 		var next = array[array.length - 2] + array[array.length - 1];
 		if (next <= 4000000) {
@@ -39,48 +38,43 @@ function evenFib() {
 			fibSeq();
 		}
 	}
-
 	function evenNums(num) {
 		if (num % 2 === 0) {
 			return num;
 		}
 	}
-
 	fibSeq();
 	return array.filter(evenNums);
 }
 
 
 
-//Count through every number
-//Target number
-//Multiply numbers less than target
-//If no multiples equal target, store number as prime
-
 
 //Largest Prime Factor
-function largePrimeFactor(product) {
-	function findPrime() {
+function largestPrimeFactor(product) {
+
+	function primeNumbers() {
 		var all        = [],
 		    prime      = [],
 		    counter    = 0;
-
 		//Find all proceding numbers of product divided by 2
 		for (var i = 1; i <= Math.ceil(product / 2); i++) {
 			all.push(i);
 		}
-
-		//Find prime numbers in "all" array
 		for (var j = 0; j < all.length; j++) {
+			//Targets a number
 			var target  = all[j];				
 			for (var k = 0; k < target; k++) {
+				//Selects number to compare to target
 				var current = all[k];
+				//1 isn't a prime number
 				if (target === 1) {
 					counter += 1;
+				//Finds numbers that aren't prime
 				} else if (target % current === 0 && target != current && current != 1) {
 					counter += 1;
-					console.log("Target: " + target + "; Current is " + current +
-						        "; Counter: " + counter);
+					/*console.log("Target: " + target + "; Current is " + current +
+						        "; Counter: " + counter);*/
 				}
 			}
 			if (counter === 0) {
@@ -89,9 +83,32 @@ function largePrimeFactor(product) {
 				counter = 0;
 			}
 		}
-		console.log(prime);
+		return prime;
 	}
-	findPrime();
+
+	function primeFactors(array) {
+		var numbers = [];
+		for (var i = 0; i < array.length; i++) {
+			var number = array[i];
+			if (product % number === 0) {
+				numbers.push(number);
+			}
+		}
+		return numbers;
+	}
+
+
+	function largestFactor(curr, next) {
+		if (next > curr) {
+			return next;
+		} else {
+			return curr;
+		}
+	}
+
+	var factors = primeFactors(primeNumbers());
+
+	console.log(factors.reduce(largestFactor));
 }
 
 
@@ -103,7 +120,7 @@ DOM
 var solutions = {
 	prob1: sum(findMultiples()),
 	prob2: sum(evenFib()),
-	prob3: largePrimeFactor(10)
+	prob3: largestPrimeFactor(13195)
 }
 
 var buttons = document.getElementsByClassName('btn');
